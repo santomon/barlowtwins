d796e95c2197b397b7fed35cf7cfbe1368606120
Quang <qa12_8@yahoo.de>
2021-12-14 15:41:19 +0100

    barlow norm.py

---
+++ norm.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import os
+import PIL
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', help='directory for data')
+args = parser.parse_args()
+
+
+class SartoriusNormDataSet(data.Dataset):
+    def __init__(self, dpath):
+        self.dpath = dpath
+        self.img_list = os.listdir(dpath)
+
+    def __getitem__(self, index):
+        return PIL.Image.open(self.dpath + self.img_list[index])
+
+    def __len__(self):
+        return len(self.dpath)
+
+
+if __name__ == '__main__':
+    dataset = SartoriusNormDataSet(args.data_dir)
+    dataloader = data.DataLoader(dataset, batch_size=len(dataset), shuffle=True, num_workers=1)
+    data = next(iter(dataloader))
+    mean = data.mean([1, 2])
+    std = data.std([1, 2])
+    print(mean, std)
+    
