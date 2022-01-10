6e410304e661076ad4d66cc84bc88575e8dded0c
Quang <qa12_8@yahoo.de>
2022-01-10 19:28:23 +0100

    barlow twins for torch; maskrcnn resnet50 fpn

--- main_torch.py
+++ main_torch.py
@@ -263,7 +263,7 @@ class BarlowTwins(nn.Module):
                 if k == "0":
                     concurrent = nn.AvgPool2d(2)(x[k] * 0)
                 else:
-                    concurrent = nn.AvgPool2d(2)(x[k] * 0) + concurrent
+                    concurrent = nn.AvgPool2d(2)(x[k] * 0 + concurrent)
         return x["pool"] + concurrent
 
 
